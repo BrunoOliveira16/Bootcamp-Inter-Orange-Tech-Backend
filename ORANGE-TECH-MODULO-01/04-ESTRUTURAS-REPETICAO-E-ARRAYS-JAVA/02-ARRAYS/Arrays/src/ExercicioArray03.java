65efe5beabb8419f7140a20158e7e4a925c4ca23
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2023-02-06 16:51:01 -0300

    Feat: atualizacao exercicio arrays - Java

--- ORANGE-TECH-MODULO-01/04-ESTRUTURAS-REPETICAO-E-ARRAYS-JAVA/02-ARRAYS/Arrays/src/ExercicioArray03.java
+++ ORANGE-TECH-MODULO-01/04-ESTRUTURAS-REPETICAO-E-ARRAYS-JAVA/02-ARRAYS/Arrays/src/ExercicioArray03.java
@@ -1,3 +1,31 @@
+import java.util.Random;
+
 public class ExercicioArray03 {
 
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.print("Numeros Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print(numero + " ");
+        }
+
+        System.out.print("\nSucessores dos Numeros Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero + 1) + " ");
+        }
+
+        System.out.print("\nAntecessores dos Numeros Aleatórios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero - 1) + " ");
+        }
+    }
 }
